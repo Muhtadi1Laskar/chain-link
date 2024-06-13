bfaeaefe2b9bfd137b102d096aa9feb8ba3fd4e8
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-06-13 13:43:07 +0600

    Added append() method of Chain Link

--- chain/chain.js
+++ chain/chain.js
@@ -11,3 +11,36 @@ class Node {
     }
 }
 
+class Chain {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    isEmpty() {
+        return !this.head;
+    }
+
+    append(value) {
+        if(value.length === 0) {
+            return 'Value cannot be empty';
+        }
+        const newNode = new Node(value);
+
+        if(this.isEmpty()) {
+            this.head = newNode;
+            this.tail = this.head;
+            this.length += 1;
+            return;
+        }
+        let prevHash = this.tail.hash;
+
+        this.tail.next = newNode;
+        this.tail = newNode;
+        newNode.previousHash = prevHash;
+        this.length++;
+
+        return;
+    }
+}
